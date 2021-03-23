3948e28ab27dde541549db404ead4192ccad0517
Henry Markle <81094687+HenryMarkle@users.noreply.github.com>
2021-03-23 17:52:59 +0300

    Update Command.java
    A tiny documentation

--- src/engine/Command.java
+++ src/engine/Command.java
@@ -1,9 +1,14 @@
 package engine;
 
+/*
+	Next goal is to change String[] arguments member to an enum type and
+	make Command class implement the Runnable interface.
+*/
+
 public abstract class Command {
-	public final String name;
-	public final int argNum;
-	public final String[] arguments;
+	public final String name;		// The name of the command
+	public final int argNum;		// How many arguments
+	public final String[] arguments;	// Description of every argument
 	
 	public abstract boolean execute(String[] line, Modifyable data);
 	

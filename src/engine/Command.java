ce18ec43142b6d46edf88c4c77af74a1fb4eeec4
Henry Markle <81094687+HenryMarkle@users.noreply.github.com>
2021-03-23 18:40:33 +0300

    Update Command.java

--- src/engine/Command.java
+++ src/engine/Command.java
@@ -1,13 +1,26 @@
 package engine;
 
 /*
+	For every command to be executed in Engine, it must extend the abstract Command class
+	and override the execute() function.
+	
+	The class has the follwing members:
+		
+		- Name: the name the command is identified with; used for matching.
+		
+		- Number of arguments: initially used to pre-verify that the player's
+		command has enough arguments for the function to execute, not used, currently.
+		
+		- An array of strings to give a breif desription of every argument the function takes.
+		Note: will be changed to an enum type.
+
 	Next goal is to change String[] arguments member to an enum type.
 	Can't implement the Runnable interface without ruining the execute() function.
 */
 
 public abstract class Command {
-	public final String name;		// The name of the command
-	public final int argNum;		// How many arguments
+	public final String name;			// The name of the command
+	public final int argNum;			// How many arguments
 	public final String[] arguments;	// Description of every argument
 	
 	public abstract boolean execute(String[] line, Modifyable data);

9898178d965b811f39c98198dd5888a6a31cee73
Henry Markle <81094687+HenryMarkle@users.noreply.github.com>
2021-04-05 23:11:18 +0300

    Adding Experimental changes

---
+++ SandBox_C#/SBLib/Tools.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace SBLib
+{
+    public static class Tools
+    {
+        public static void Rename (BaseObject obj, string newname) => obj.Name = newname;
+
+               
+
+        
+
+    }
+}
